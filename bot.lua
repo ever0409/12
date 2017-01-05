b1a9214a2a4b3bda7a72ee99f4ad54f7343d193e
Mr.Nitro <sajad0409@gmail.com>
2017-01-06 00:46:39 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -6,8 +6,7 @@ package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 tdcli = dofile('tdcli.lua')
 redis = (loadfile "./libs/redis.lua")()
 sudo_users = {
-  238773538,
-  173606679,
+  198794027,
   0
 }
 
